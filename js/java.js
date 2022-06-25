0fa11b6041fc8647766433beae8dc1f1c0d73d9c
Артур Лапин <comini2012@gmail.com>
2022-06-25 11:33:03 +0300

    Не доделанный сайт для курсача

---
+++ js/java.js
@@ -0,0 +1,26 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [48.00878157, 37.79983749],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [48.00878157, 37.79983749]
+            },
+            properties: {
+                iconContent: 'Клуб Hazzer',
+                hintContent: 'С 20:00 до 5:00'
+            }
+        }, {
+            preset: 'islands#blackStretchyIcon',
+            draggable: false
+        })
+
+    myMap.geoObjects.add(myGeoObject);
+}
